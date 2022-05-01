1e5fa3de515b736454f67a3c7ac194ded1f91727
Anusha Nambiar <anambiar7200@gmail.com>
2022-05-01 13:37:00 -0400

    modified image_recog for picamera

--- image_recog.py
+++ image_recog.py
@@ -1,15 +1,20 @@
 import cv2
+import io
+import picamera
+import numpy
 
 
-#stream = io.ByestIO()
+stream = io.ByestIO()
 
-# with picamera.PiCamera() as camera: 
-#   camera.capture(stream, format='jpeg')
+with picamera.PiCamera() as camera: 
+  camera.capture(stream, format='jpeg')
+
+
 haar_path = (cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 cascade = cv2.CascadeClassifier(haar_path)
 
-#img = cv2.imdecode(numpy.frombuffer(stream.getvalue(), dtype=numpy.uint8, 1)
-capture = cv2.VideoCapture(0)
+img = cv2.imdecode(numpy.frombuffer(stream.getvalue(), dtype=numpy.uint8, 1)
+#capture = cv2.VideoCapture(0)
 
 while True: 
   rect, img = capture.read()
@@ -21,7 +26,8 @@ while True:
   for (x,y,w,h) in faces:
       cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),4)
 
-  cv2.imshow('img',img)
+  cv2.imwrite('result.jpg',img)
+  cv2.imshow("result", img)
     
   # Wait for Esc key to stop
   k = cv2.waitKey(30) & 0xff
